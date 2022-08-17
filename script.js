c6d07a66c048a9ac33614971a022b412bb510415
Charmelle Gonzales <charmelle.gonzales@gmail.com>
2022-08-16 20:07:45 -0500

    Add color hover effect

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 // query selectors
 const container = document.querySelector(".container")
-let container2 = document.createElement("div");
-let div = document.createElement("div")
-let div2 = document.createElement("div")
+let columnDiv;
+let squareDiv;
 
 // test user imput
 let userInput = Number(prompt("Enter Grid Size"))
 createGrid(userInput);
+const squareDivs = document.querySelectorAll('.squareDiv')
 
+function startColor(){
+    squareDivs.forEach((squareDiv) => {
+        squareDiv.addEventListener('mouseover', function(e) {
+            e.target.style.backgroundColor = "black";
+        })
+    })
+}
 //create a 16x16 grid
 // for loop to create 16 divs
 function createGrid(gridNum) {
     limitX = gridNum
     limitY = gridNum
-    for (let y = 0; y < limitY; y++) {
-        container2 = document.createElement("div");
-        container.appendChild(container2)
-        for (let x = 0; x < limitX; x++){
-                div = document.createElement("div")
-                applyStyle();
-                container2.appendChild(div);
+    for (let y = 1; y <= limitY; y++) {
+        columnDiv = document.createElement("div");
+        columnDiv.classList.add('columnDiv');
+        container.appendChild(columnDiv)
+        for (let x = 1; x <= limitX; x++){
+                squareDiv = document.createElement("div")
+                squareDiv.classList.add(`squareDiv`)
+                applyDivStyle();
+                columnDiv.appendChild(squareDiv);
             }
     }
+
 }
 
-//function for div style
-function applyStyle() {
-    div.style.display = "block";
-    div.style.border = "1px solid black"
-    div.style.width = "25px"
-    div.style.height = "25px"
+function changeColor() {
+    squareDiv.style.backgroundColor = "black";
 }
+//function for div style
+function applyDivStyle() {
+    squareDiv.style.display = "block";
+    squareDiv.style.border = "1px solid black"
+    squareDiv.style.width = "25px"
+    squareDiv.style.height = "25px"
+}
+
+startColor();

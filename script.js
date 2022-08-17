6bbdad627256c07a30e7f2b6d40601d28ff39655
Charmelle Gonzales <charmelle.gonzales@gmail.com>
2022-08-17 15:35:36 -0500

    Change grid style and structure

--- script.js
+++ script.js
@@ -1,85 +1,61 @@
 // query selectors
 const container = document.querySelector(".container")
 const createBtn = document.querySelector('#create-btn')
+const sizeEl = document.querySelector('#grid-size')
 
+// global variables
+let userInput
+let defaultSize = 16;
 
-let userInput;
-let columnDiv
-let squareDiv
-
+function populate(size) {
+    container.style.setProperty('--size', size)
+    for (let i = 0; i < size*size; i++){
+        const div = document.createElement('div')
+        div.classList.add('pixel')
+        container.appendChild(div)
+    }
+}
 //display Start UI
-displayStartGrid();
-changeColors();
+populate(defaultSize)
+changeColors()
 
 //trigger create new grid
-createBtn.addEventListener('click', start);
+createBtn.addEventListener('click', createNewGrid);
 
 // game functions
-function start() {
-    createNewGrid();
-    changeColors();
+function createNewGrid() {
+    removeSquares()
+    getUserInput()
+    // check userinput is valid
+    if (userInput > 100 || !userInput | isNaN(userInput)){
+        populate(defaultSize);
+        return alert('Please enter a number between 1-100')
+    }
+    else {
+        populate(userInput)
+        changeColors()
+
+    }
 }
 
+function getUserInput() {
+    userInput = Number(sizeEl.value);
+    return userInput;
+}
+    
+   
+function removeSquares() {
+    while(container.firstChild) {
+     container.removeChild(container.firstChild)
+    }
+    
+ }
 function changeColors(){
-    const squareDivs = document.querySelectorAll('.squareDiv')
+    const squareDivs = document.querySelectorAll('.pixel')
     squareDivs.forEach((squareDiv) => {
         squareDiv.addEventListener('mouseover', function(e) {
             e.target.style.backgroundColor = "black";
         })
     })
-}
-
-function displayStartGrid() {
-    for (let y = 1; y <= 16; y++) {
-        columnDiv = document.createElement("div")
-        columnDiv.classList.add('columnDiv')
-        container.appendChild(columnDiv)
-        for (let x = 1; x <= 16; x++){
-                squareDiv = document.createElement("div")
-                squareDiv.classList.add(`squareDiv`)
-                applyDivStyle();
-                columnDiv.appendChild(squareDiv)
-            }
-    }
-}
-
-function createNewGrid() {
-    removeSquares()
-    userInput = Number(document.getElementById("user-input").value)
-    console.log(typeof(userInput))
-    if (userInput > 100 || !userInput){
-        displayStartGrid()
-        return  alert('Please enter a number between 1-100')
-    }
-    let gridSize = userInput
-    console.log(userInput)
-    for (let y = 1; y <= gridSize; y++) {
-        columnDiv = document.createElement("div")
-        columnDiv.classList.add('columnDiv')
-        container.appendChild(columnDiv)
-        for (let x = 1; x <= gridSize; x++){
-                squareDiv = document.createElement("div")
-                squareDiv.classList.add(`squareDiv`)
-                applyDivStyle();
-                columnDiv.appendChild(squareDiv)
-            }
-    }
-
-}
-
-function removeSquares() {
-   while(container.firstChild) {
-    container.removeChild(container.firstChild)
-   }
-   
-}
-
-function applyDivStyle() {
-    squareDiv.style.display = "block"
-    squareDiv.style.visibility = "visible"
-    squareDiv.style.border = "1px solid black"
-    squareDiv.style.width = "25px"
-    squareDiv.style.height = "25px"
-    
 }
 

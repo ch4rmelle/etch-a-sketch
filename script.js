e84d34cfa299043de81671551e4a6d63684f60cb
Charmelle Gonzales <charmelle.gonzales@gmail.com>
2022-08-17 12:36:54 -0500

    Add removeSqauares functionality

--- script.js
+++ script.js
@@ -1,48 +1,80 @@
 // query selectors
 const container = document.querySelector(".container")
-let columnDiv;
-let squareDiv;
+const createBtn = document.querySelector('#create-btn')
 
-// test user imput
-let userInput = Number(prompt("Enter Grid Size"))
-createGrid(userInput);
-const squareDivs = document.querySelectorAll('.squareDiv')
 
-function startColor(){
+let userInput;
+let columnDiv
+let squareDiv
+
+//display Start UI
+displayStartGrid();
+changeColors();
+
+//trigger create grid
+createBtn.addEventListener('click', start);
+
+function start() {
+    createNewGrid();
+    changeColors();
+}
+function changeColors(){
+    const squareDivs = document.querySelectorAll('.squareDiv')
     squareDivs.forEach((squareDiv) => {
         squareDiv.addEventListener('mouseover', function(e) {
             e.target.style.backgroundColor = "black";
         })
     })
 }
-//create a 16x16 grid
-// for loop to create 16 divs
-function createGrid(gridNum) {
-    limitX = gridNum
-    limitY = gridNum
-    for (let y = 1; y <= limitY; y++) {
-        columnDiv = document.createElement("div");
-        columnDiv.classList.add('columnDiv');
+
+function displayStartGrid() {
+    for (let y = 1; y <= 16; y++) {
+        columnDiv = document.createElement("div")
+        columnDiv.classList.add('columnDiv')
         container.appendChild(columnDiv)
-        for (let x = 1; x <= limitX; x++){
+        for (let x = 1; x <= 16; x++){
                 squareDiv = document.createElement("div")
                 squareDiv.classList.add(`squareDiv`)
                 applyDivStyle();
-                columnDiv.appendChild(squareDiv);
+                columnDiv.appendChild(squareDiv)
             }
     }
 
 }
-
-function changeColor() {
-    squareDiv.style.backgroundColor = "black";
+//create a 16x16 grid
+// for loop to create 16 divs
+function createNewGrid() {
+    removeSquares()
+    userInput = document.getElementById("user-input").value;
+    let size = Number(userInput)
+    console.log(userInput)
+    for (let y = 1; y <= size; y++) {
+        columnDiv = document.createElement("div")
+        columnDiv.classList.add('columnDiv')
+        container.appendChild(columnDiv)
+        for (let x = 1; x <= size; x++){
+                squareDiv = document.createElement("div")
+                squareDiv.classList.add(`squareDiv`)
+                applyDivStyle();
+                columnDiv.appendChild(squareDiv)
+            }
+    }
 }
+
+function removeSquares() {
+   while(container.firstChild) {
+    container.removeChild(container.firstChild)
+   }
+   
+}
+
 //function for div style
 function applyDivStyle() {
-    squareDiv.style.display = "block";
+    squareDiv.style.display = "block"
+    squareDiv.style.visibility = "visible"
     squareDiv.style.border = "1px solid black"
     squareDiv.style.width = "25px"
     squareDiv.style.height = "25px"
+    
 }
 
-startColor();

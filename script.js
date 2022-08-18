4c09f5f1ac72fb4add58cd89b0b5f6d9cced1438
Charmelle Gonzales <charmelle.gonzales@gmail.com>
2022-08-17 19:20:38 -0500

    Add color effect

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 const container = document.querySelector(".container")
 const createBtn = document.querySelector('#create-btn')
 const sizeEl = document.querySelector('#grid-size')
+const funBtn = document.querySelector('#fun-btn');
 
 // global variables
 let userInput
 let defaultSize = 16
 let counter = 0
+let randomRGB = generateRGB();
+let brightness = 110;
+
 
 function populate(size) {
     container.style.setProperty('--size', size)
@@ -18,10 +22,11 @@ function populate(size) {
 }
 //display Start UI
 populate(defaultSize)
-changeColors()
+changeBlack()
 
 //trigger create new grid
 createBtn.addEventListener('click', createNewGrid);
+funBtn.addEventListener('click', changeColors);
 
 // game functions
 function createNewGrid() {
@@ -34,8 +39,7 @@ function createNewGrid() {
     }
     else {
         populate(userInput)
-        changeColors()
-
+        changeBlack()
     }
 }
 
@@ -44,36 +48,40 @@ function getUserInput() {
     return userInput;
 }
     
-   
 function removeSquares() {
     while(container.firstChild) {
      container.removeChild(container.firstChild)
     }
     
  }
+function changeBlack() {
+    const squareDivs = document.querySelectorAll('.pixel')
+    squareDivs.forEach((squareDiv) => {
+        squareDiv.addEventListener('mouseover', function(e) {
+            e.target.style.backgroundColor = "black";
+        })
+    })
+}
+
 function changeColors(){
     const squareDivs = document.querySelectorAll('.pixel')
     squareDivs.forEach((squareDiv) => {
         squareDiv.addEventListener('mouseover', function(e) {
-            counter++
-            if (counter % 10 === 0) {
-                e.target.style.backgroundColor = "black" ;
-            } else {
-                        e.target.style.backgroundColor = rgbChange() ;
-                    }
-                })
-            })
+            e.target.style.backgroundColor = randomRGB
+            brightness = brightness - 10
+            if(brightness < 0)
+            {
+                brightness = 100
+            }
+            e.target.style.filter = `brightness(${brightness}%)`
+            console.log(brightness)
+        })
+    })}
 
-        }
-        
-function rgbChange() {
-    const rgb = ["red", "green", "yellow", "orange"]
-    color = rgb[Math.floor(Math.random()*rgb.length)]
+function generateRGB() {
+    const rgb = Math.floor(Math.random()*16777215).toString(16)
+    color = "#" + rgb;
+    console.log(color)
     return color;
 }
-
-// function hoverCount() {
-//     counter++
-//     return counter
-// }
 
